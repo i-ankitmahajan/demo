e7e8e2e74c74dc78d8a4fa49ff7530460924aece
Ankit Mahajan <ankit_mahajan@intuit.com>
2020-05-25 23:07:51 +0530

    Added project

---
+++ src/main/java/com/ankit/testing/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.ankit.testing.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+    private Logger LOG = LoggerFactory.getLogger(getClass());
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Hello World!!!");
+    }
+}
